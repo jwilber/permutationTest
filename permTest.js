91a27aea252b0da4c7dcff09132f2590b6ba6dc4
Jared Wilber <jdwlbr@gmail.com>
2019-01-27 01:26:52 -0700

    added transitionSevendown and cleaned up code

--- permTest.js
+++ permTest.js
@@ -24,7 +24,6 @@ function loop() {
 const svgD3 = d3.select('svg');
 const width = svgD3.node().getBoundingClientRect().width;
 const height = svgD3.node().getBoundingClientRect().height;
-console.log(height)
 const margin = 20;
 
 const initialScale = 0.01;
@@ -37,14 +36,6 @@ const cntrlCenter = width / 1.5;
 const heightMuCenter = height / 1.8;
 
 const radius = 5;
-
-  // function changeNetwork2() {
-  //     d3.selectAll('.dotResponse')
-  //       .attr('transform', function(d) {
-  //         // console.log(d);
-  //         return `translate(${d.x}, ${Math.min(d.value, height - radius - 1)})`
-  //       })
-  //   }
 
 //////////////////////////
 ///// node functions /////
@@ -122,12 +113,13 @@ const nodeInitialYPlacement = (d) => {
     
 
   // dsn dots need to live off-screen, until the final dsn build (so don't mess w/ other nodes)
-      let sampleData = d3.range(350).map((d,i) => ({r: 40 - i * 0.5,
+      let sampleData = d3.range(250).map((d,i) => ({r: 40 - i * 0.5,
                                                     value: width/2 + d3.randomNormal(0, 1.5)() * 50,
                                                     nodeGroup: i <= 23 ? 'llama' : i <= 39 ? 'resp' : 'dsn',
                                                     dotValue: i % 2 === 0 ? 
-                                                      d3.randomNormal(8, 2.5)().toFixed(1): 
-                                                      d3.randomNormal(4.5, .75)().toFixed(1)
+                                                      d3.randomNormal(7, 2.5)().toFixed(1): 
+                                                      d3.randomNormal(4.5, .75)().toFixed(1),
+                                                    'permDsn': d3.randomNormal(0, 1)().toFixed(1)
                                                     }));
   
   // set params for force layout
@@ -153,28 +145,12 @@ const nodeInitialYPlacement = (d) => {
     .attr('class', d => d.nodeGroup === 'llama' ? 'dot' : 'dotResponse')
     .attr('group', (d,i) => i % 2 == 0 ? 'true' : 'false');
 
-  d3.selectAll('.dotResponse')
-    .append('circle')
-    .style('opacity', .75)
-    .attr('fill' ,'pink')
-    .attr('r', 0)
-    .attr('stroke-width', .51)
-    .attr('stroke', 'black')
-    // .attr('testStatGroup', function(d,i) {
-    //   if (d.nodeGroup === 'resp') {
-    //     testStatClass = 'testStat'.concat(i)
-    //     d3.select(this).classed(testStatClass, true)
-    //   } else {
-    //     d3.select(this).classed('testStatDsn', true)
-    //   }
-    // })
-
   function changeNetwork() {
     d3.selectAll('g.dot')
       .attr('transform', d=> `translate(${d.x}, ${d.y})`)
 
-    d3.selectAll('g.dotResponse')
-      .attr('transform', d=> `translate(${d.x}, ${d.y})`)
+    // d3.selectAll('g.dotResponse')
+    //   .attr('transform', d=> `translate(${d.x}, ${d.y})`)
   }
   
   function loadRoughsvgD3(svgD3Data) {
@@ -293,7 +269,7 @@ function shuffleTestStat(nodePositions, responseNode) {
   d3.selectAll(responseNode)
     .transition()
     .duration(800)
-    .attr('r', d => d.radius / 1.2)
+    .attr('r', d => d.radius / 1.05)
 }
 
 
@@ -307,26 +283,6 @@ function moveNodes() {
   force.force('y', d3.forceY().strength(1).y(nodeTreatmentHeight))
   .force('collision', d3.forceCollide(nodeGroupMoveForceCollide))
   force.alpha(.1).restart();
-}
-
-function changeNetwork2() {
-      d3.selectAll('.dotResponse')
-        .attr('transform', function(d) {
-          // console.log(d);
-          return `translate(${d.x}, ${Math.min(d.y, height - 500 - 1)})`
-        })
-    }
-
-
-function moveHist(){
-  force.force('center', null)
-  .force('collision', d3.forceCollide(d => 10).strength(1))
-  .alphaDecay(.025) 
-  // .velocityDecay(.5)
-    .force('x', d3.forceX((d,i) => d.value).strength(1.5))
-    .force('y', d3.forceY(height - 500).strength(1.5))
-    .on('tick', changeNetwork2)
-    force.alpha(.5).restart()
 }
 
 function showAllTestDsnNodes() {
@@ -404,46 +360,10 @@ let controlTitle = svgD3.append('text')
   // stuff for distribution
   //x scales
 const x = d3.scaleLinear()
-    .domain(d3.extent(sampleData.filter(d => d.nodeGroup === 'dsn'), d => +d.dotValue))
+    .domain(d3.extent(sampleData.filter(d => d.nodeGroup === 'dsn'), d => +d.permDsn))
     .rangeRound([width/4, width/1.5]); // hist width(left, right)
 
-const nbins = 25;
-
-
-  // let allData = d3.range(1000).map((d,i) => ({r: 40 - i * 0.5,
-  //                  Value: width/2 + d3.randomNormal(0, 1.5)() * 50,
-  //                  nodeGroup: i <= 23 ? 'llama' : i <= 39 ? 'resp' : 'dsn',
-  //                  dotValue: i % 2 === 0 ? 
-  //                            d3.randomNormal(8, 2.5)().toFixed(1): 
-  //                            d3.randomNormal(4.5, .75)().toFixed(1)}));
-
-
-
-//number of bins for histogram
-function showCircles(){
-  d3.selectAll('circle.histCirc')
-    .transition()
-    .duration(800)
-    .attr("r", (d,i) => (i < 4 && d.dataIndex < 5) ? d.radius/1.2 : 0)
-}
-
-function showSecondResponseNode(){
-  // i controls row, d.dataIndex is count in row, left-to-right
-  d3.selectAll('circle.histCirc')
-      // .filter((d,i) => (i < 10 && d.dataIndex < 15))
-    // .enter()
-    .transition()
-    // .append('circle')
-    .attr("r", (d,i) => (i < 20) ? d.radius/1.2 : d.radius / 10.2)
-    // .attr("visibility", (d,i) => (i < 10 && d.dataIndex < 15) ? 'hidden' : 'visible')
-}
-
-// .attr('testStatGroup', function(d,i) {
-//           if (i < 1 && d.dataIndex == 12) {
-//             d3.select(this).classed('response1', true)
-//           } else if (i < 1 && d.dataIndex == 9) {
-//             d3.select(this).classed('response2', true)
-//           }
+const nbins = 18;
 
 function assignResponseNodes(d,i) {
   'testStat'.concat(i)
@@ -453,17 +373,14 @@ function assignResponseNodes(d,i) {
 }
 
 function update(){
-  // force.stop()
 
-    // let data = sampleData.filter(d => d.nodeGroup === 'dsn')
-    //       .slice(startNum, endNum);
     let data = sampleData.filter(d => d.nodeGroup === 'dsn')
 
     //histogram binning
     const histogram = d3.histogram()
       .domain(x.domain())
       .thresholds(x.ticks(nbins))
-      .value(d => d.dotValue);
+      .value(d => d.permDsn);
 
     //binning data and filtering out empty bins
     const bins = histogram(data);
@@ -488,14 +405,14 @@ function update(){
                   dataIndex: i,
                   value: p.Value,
                   radius: (x(d.x1)-x(d.x0))/1.9
+                  // permDsn: d.permDsn
                 }
         }))
       .enter()
       .append("circle")
       .attr('class', 'histCirc')
-      .attr('rowIndex', (d,i) => i)
-      .attr('circleIndex', (d,i) => d.dataIndex * (i + 1))
-      .attr('testStatGroup', function(d,i) {
+      .attr('testStatValue', d => d.permDsn)
+      .attr('', function(d,i) {
         if (i < 1 && d.dataIndex == 12) {
           d3.select(this).classed('response1', true)
         } else if (i < 1 && d.dataIndex == 9) {
@@ -514,10 +431,15 @@ function update(){
           d3.select(this).classed('response8', true)
         } else if (i < 1 && d.dataIndex == 2) {
           d3.select(this).classed('response9', true)
-        } else if (i < 1 && d.dataIndex == 10) {
-          d3.select(this).classed('response10', true)
         } else {
           d3.select(this).classed('histogramNode', true)
+        }
+      })
+      .attr('', function(d) {
+        if (d.dataIndex > 19) { // determine which test-stat nodes to highlight
+          d3.select(this).classed('extreme', true)
+        } else {
+          d3.select(this).classed('notExtreme', true)
         }
       })
       .transition()
@@ -527,8 +449,6 @@ function update(){
         .transition()
           .duration(800)
           .attr('r', d => 0)
-          // .attr("r", (d,i) => (i < 1 && d.dataIndex == 12) ? d.radius/1.2 : 0)
-          // .attr('visibility', (d,i) => (i < 1 && d.dataIndex == 12) ? 'visible' : 'hidden')
           .style('opacity', 1)
           .attr('fill' ,'pink')
           .transition()
@@ -549,7 +469,6 @@ function update(){
 
 
     //UPDATE old elements present in new data.
-//     dots.attr("class", "update");
 
     //ENTER new elements present in new data.
     dots.enter()
@@ -565,78 +484,6 @@ function update(){
           .attr("r", function(d) {
           return (d.length==0) ? 0 : d.radius; })
 };//update
-
-
-
-
-// //draw everything
-// update(0);
-// let clicked = 1;
-// d3.select('#clickMe')
-//   .on('click', function() {
-//     update(clicked);
-//     clicked == 0 || clicked == 1 ? clicked = clicked + 1 : clicked = 200;
-//   })
- 
-
-
-
-    function moveToDistribution(){ 
-      force.stop()
-      // d3.selectAll('.testStatDsn')
-      //   .transition()
-      //   .duration(1000)
-      //   .style('fill', 'blue')
-      //   .attr('r', 0)
-
-      // problem:
-      // I want to move each dot to it's location
-
-
-
-        //histogram binning
-        const histogram = d3.histogram()
-          .domain(x.domain())
-          .thresholds(x.ticks(nbins))
-          .value(d => +d.dotValue);
-
-        //binning data and filtering out empty bins
-        const bins = histogram(sampleData.filter(d => d.nodeGroup === 'dsn'));
-
-        // console.log('test hist application');
-        // d3.selectAll('.testStatDsn')
-        //   .each(function(d,i) {
-        //     console.log(i);
-        //     console.log(histogram(d))
-        //   })
-
-        //g container for each bin
-        let binContainer = svgD3
-          .append('g')
-          .attr('transform', `translate(100, ${-height/2})`)
-          .selectAll("g.gBin")
-          .data(bins)
-          .enter()
-          .append("g")
-          .attr("class", "gBin")
-          .attr("transform", d => `translate(${x(d.x0)}, ${height})`);
-
-          binContainer.selectAll(".preHistPosit")
-            .data(d => d.map((p, i) => {
-                return {value: p.Value,
-                        radius: (x(d.x1)-x(d.x0))/2.5}
-            }))
-            .enter()
-            .append("circle")
-            .attr('class', 'histCirc')
-            .transition()
-            .attr("cx", 0) //g element already at correct x pos
-            .attr("cy", (d, i) => {
-                return - i * 2 * d.radius - d.radius})
-            .attr("r", d => d.radius/2);
-      
-    };//moveToDistribution
-
 
 
 //////////////////////////////////////////////////
@@ -686,8 +533,6 @@ function transitionOneDown() {
 
   update()
 
-  // init dsn chart, but make them all invisible
-  // update(0,250);
 }
 
 function transitionTwoUp() {
@@ -730,7 +575,6 @@ function transitionTwoDown() {
     .transition()
     .delay(0)
 
-  // showCircles();
 }
 
 function transitionThreeUp() {
@@ -760,7 +604,7 @@ function transitionThreeDown() {
 
   d3.selectAll('circle.response1')
     .transition()
-    .attr('r', d => d.radius / 1.2)
+    .attr('r', d => d.radius / 1.05)
 
 }
 
@@ -782,7 +626,7 @@ function transitionFourDown() {
   // HIST ENTER
   d3.selectAll('circle.response2')
     .transition()
-    .attr('r', d => d.radius / 1.2)
+    .attr('r', d => d.radius / 1.05)
 
   // TODO, calculate desired test-statistic value. (left response - right response)
   // give it to .teststat0.testStat as an attribute
@@ -795,13 +639,6 @@ function transitionFiveUp() {
   d3.selectAll('.responseText').transition().duration(1600).attr('y', 65.2) 
   // re-add group titles
   d3.selectAll('.groupTitle').transition().delay(1400).attr('visibility', 'visible')
-
-  // do stuff with test-statistic nodes
-  // d3.selectAll('.testStatDsn')
-  //   .transition()
-  //   .duration(2000)
-  //   .attr('transform', `translate(0, 0)`)
-  //   .attr('r', 0)
 
   // remove histogram nodes
   d3.selectAll('circle.histogramNode')
@@ -823,8 +660,6 @@ function transitionFiveUp() {
     .on('tick', changeNetwork)
   force.alpha(.1).restart();
 
-  setTimeout(showAllTestDsnNodes, 2200);
-
 }
 
 
@@ -845,16 +680,7 @@ function transitionFiveDown() {
     function() { shuffleTestStat(nodeRandomPosSix, '.response7') },
     function() { shuffleTestStat(nodeRandomPosSeven, '.response8') },
     function() { shuffleTestStat(nodeRandomPosEight, '.response9') },
-    // function() { shuffleTestStat(nodeRandomPosNine, 'response3') },
-    // function() { shuffleTestStat(nodeRandomPosTen, 'response3') },
-    // function() { shuffleTestStat(nodeRandomPosEight, 'response3') }, 
     );
-
-  // setTimeout(showAllTestDsnNodes, 3200);
-  // setTimeout(update(5, 200), 3200)
-
-
-  // HIST ENTER
 }
 
 function transitionSixUp() {
@@ -867,15 +693,11 @@ function transitionSixDown() {
   d3.selectAll('.responseText').transition().duration(2000).attr('y', -2000) 
   d3.selectAll('.groupTitle').transition().delay(1400).attr('visibility', 'hidden')
 
-  
-
   // move nodes to distribution
-  // moveHist()
-  // moveToDistribution();
   d3.selectAll('circle.histogramNode')
     .transition()
     .duration(1400)
-    .attr('r', d => d.radius / 1.2)
+    .attr('r', d => d.radius / 1.05)
 
   svgD3.append("g")
   .attr("class", "axis axis--x")
@@ -883,6 +705,17 @@ function transitionSixDown() {
   .call(d3.axisBottom(x));
 }
 
+function transitionSevenDown() {
+  d3.selectAll('circle.extreme')
+    .transition()
+    .duration(500)
+    .attr('r', d => d.radius / .9)
+    .attr('stroke-width', .5)
+    .attr('fill', 'coral')
+    .transition()
+    .attr('r', d => d.radius / 1.05)
+    .attr('stroke-width', 0.3)
+}
 
 function calculateTestStatistic() {
   let testStatistics = d3.selectAll('.dot')
